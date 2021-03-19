d32c26fc66324bfb6834a818ae2b98fe2df819a3
ChillyWilly <noreply>
2021-03-19 04:05:36 -0400

    view all departments and view all roles complete

--- inquirer.js
+++ inquirer.js
@@ -1,28 +1,63 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const table = require('console.table');
 const Choice = require('inquirer/lib/objects/choice');
-
-inquirer.prompt({
-    type: 'list',
-    message: 'choose an action',
-    name: 'mainAnswer',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role', 'Add employee', 'Update employee role']
-}).then(({
-    mainAnswer
-}) => {
-    switch (mainAnswer) {
-        case 'View all departments':
-            break;
-        case 'View all roles':
-            break;
-        case 'View all employees':
-            break;
-        case 'Add department':
-            break;
-        case 'Add role':
-            break;
-        case 'Add employee':
-            break;
-        case 'Update employee role':
-            break; 
-    }
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'zoWM8Amj63JW!',
+    database: 'employee_db'
 });
+
+const mainPrompt = () => {
+    inquirer.prompt({
+        type: 'list',
+        message: 'choose an action',
+        name: 'mainAnswer',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role', 'Add employee', 'Update employee role', 'Quit']
+    }).then(({
+        mainAnswer
+    }) => {
+        switch (mainAnswer) {
+            case 'View all departments':
+                connection.query('SELECT * FROM departments', (err, res) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.table(res);
+                        mainPrompt();
+                    }
+                })
+                break;
+            case 'View all roles':
+                connection.query(`SELECT roles.id, roles.title, roles.salary, departments.name AS department
+                FROM roles 
+                LEFT JOIN departments ON roles.department_id = departments.id`, (err, res) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.table(res);
+                        mainPrompt();
+                    }
+                })
+                break;
+            case 'View all employees':
+                break;
+            case 'Add department':
+                break;
+            case 'Add role':
+                break;
+            case 'Add employee':
+                break;
+            case 'Update employee role':
+                break;
+            case 'Quit':
+                console.log("Thank you!");
+                connection.end();
+                break;
+        }
+    })
+};
+
+mainPrompt();
