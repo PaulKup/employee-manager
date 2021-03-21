8bee74f5adeee7f53094c67aa535ed39aacbc15f
ChillyWilly <noreply>
2021-03-21 03:38:11 -0400

    view all employees and add department

--- inquirer.js
+++ inquirer.js
@@ -43,8 +43,44 @@ const mainPrompt = () => {
                 })
                 break;
             case 'View all employees':
+                connection.query(`SELECT employees.id, employees.first_name, employees.last_name, 
+                roles.title, roles.salary, departments.name AS department, CONCAT(e.first_name, ' ',  e.last_name) AS manager
+                FROM employees 
+                LEFT JOIN roles ON employees.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                LEFT JOIN employees AS e ON employees.manager_id = e.id;`, (err, res) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.table(res);
+                        mainPrompt();
+                    }
+                })
                 break;
             case 'Add department':
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'depName',
+                    message: 'Enter new department name: ',
+                    validate: depName => {
+                        if (depName) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(depName => {
+                    department = depName.depName;
+                    const sql = `INSERT INTO departments (name)
+                                 VALUES (?)`;
+                    const param = [department];
+                    connection.query(sql, param, function (err, res) {
+                        if (err) {
+                            throw err;
+                            }
+                        mainPrompt();
+                    })
+                });
                 break;
             case 'Add role':
                 break;
